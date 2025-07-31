2734220cde963e452df35785006be01629059807
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2025-07-31 08:29:21 +0300

    feat Q1: get/ping and Q2: get/home + home.html

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
 from fastapi import FastAPI
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/ping")
+def ping():
+    return {"pong"}
+
+
+@app.get("/home")
+def home():
+    with open("home.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=200, media_type="text/html")
+
