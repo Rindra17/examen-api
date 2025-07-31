1db6d052bb04b926d82a08f9ec2d7ebaa29e88e3
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2025-07-31 09:01:33 +0300

    feat: Q4 post/posts

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
+import datetime
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
@@ -20,3 +24,22 @@ def unknown(full_path: str):
     with open("notFound.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=404, media_type="text/html")
+
+class PostModel(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: datetime.datetime
+
+post_list: List[PostModel] = []
+
+def serialized_posts():
+    posts_serialized = []
+    for post in post_list:
+        posts_serialized.append(post.model_dump_json())
+    return posts_serialized
+
+@app.post("/posts")
+def add_players(new_post: List[PostModel]):
+    post_list.extend(new_post)
+    return JSONResponse( {"posts": serialized_posts()}, status_code=201)
